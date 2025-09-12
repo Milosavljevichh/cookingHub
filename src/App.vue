0914e1b13bc60bd4600cd95ce673ddbcf5dfe872
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2025-09-12 22:50:19 +0200

    added ajax test call

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
   import { RouterView } from 'vue-router';
+  import Header from './layout/Header.vue';
+  import Footer from './layout/Footer.vue';
+
+  //!!!NOTE trenutno se ovde dohvata api data, ali ce se pomeriti u vuex
+  let kanal = new XMLHttpRequest();
+
+  let data
+
+  kanal.open('GET', 'https://themealdb.com/api/json/v1/1/search.php?s=', true)
+  kanal.send()
+  kanal.onreadystatechange = () => {
+    if (kanal.readyState === 4 && kanal.status === 200) {
+      data = JSON.parse(kanal.responseText)
+      console.log(data)
+    }
+  }
 </script>
 
 <template>
+  <Header />
   <RouterView />
+  <Footer />
 </template>
-
---
+++ src/layout/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { ref, computed } from 'vue';
+
+    let searchText = ref('')
+    //!!!NOTE trenutno se ovde radi search ali to ce se prebaciti u vuex
+    let filterRecipes = computed(()=>{
+        let s = searchText.value
+        if (!s) return ''
+    })
+</script>
+
+<template>
+    <header>
+        <input v-model="searchText" type="text" name="searchbar" id="searchbar">
+    </header>
+</template>
+
+<style>
+    header {
+        width: 100%;
+        height: 7vh;
+        max-height: 100px;
+        background-color: var(--primary);
+    }
+</style>
